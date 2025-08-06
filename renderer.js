bc31a5baafe62dc07746eb40f00cb78f7306fb9d
5Stygian <rlcrafistohard@gmail.com>
2025-08-06 01:09:31 -0700

    make js*n linguist detectable

--- renderer.js
+++ renderer.js
@@ -1,8 +1,8 @@
 //const information = document.getElementById('info')
 //information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
-const pl = document.getElementById("testpl")
-const src = document.getElementById("testsrc")
+const pl = document.getElementById("playlists")
+const src = document.getElementById("sources")
 
 const func = async () => {
   const response = await window.versions.ping()

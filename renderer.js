be5a4dfa5cb9da6e66704b3bbf67618c237136d9
5Stygian <rlcrafistohard@gmail.com>
2025-08-05 17:19:20 -0700

    misc

--- renderer.js
+++ renderer.js
@@ -1,5 +1,8 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+//const information = document.getElementById('info')
+//information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const pl = document.getElementById("testpl")
+const src = document.getElementById("testsrc")
 
 const func = async () => {
   const response = await window.versions.ping()
@@ -7,3 +10,13 @@ const func = async () => {
 }
 
 func()
+
+const testpl = async () => {
+	console.log("playlist tab opened")
+}
+pl.addEventListener('click', testpl)
+
+const testsrc = async () => {
+	console.log("source tab opened")
+}
+src.addEventListener('click', testsrc)
